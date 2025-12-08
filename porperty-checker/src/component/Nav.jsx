1376ebe496694499647752e03980c1d1f80befa2
Rafael <rafaelfboroto@gmail.com>
2025-12-08 15:56:51 +0000

    nav

---
+++ porperty-checker/src/component/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './nav.css'
+
+function Nav() {
+    return (
+        <nav class="navbar navbar-inverse teste">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#">Property Checker</a>
+                    </div>
+                </div>
+        </nav>
+            );
+}
+
+            export default Nav
